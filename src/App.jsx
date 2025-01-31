687181984d6bf5bf4725e9255be7235057684cb4
Mursalin Habib <78700083+mursalin-habib@users.noreply.github.com>
2025-01-31 20:26:54 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Education from './components/Education';
 import Experience from './components/Experience';
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Technologies from './components/Technologies';
