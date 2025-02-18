dd210f4aadc6b187ff407b3b05192910a0af8072
Mursalin Habib <78700083+mursalin-habib@users.noreply.github.com>
2025-02-18 21:04:07 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       {/* <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div> */}
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-8 text-2xl">
         <a href="https://www.linkedin.com/in/mursalin-habib" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
           <FaLinkedin /></a>
           <a
